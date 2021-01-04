cad25fd04412da349d9cbae5ff8b492c866f857e
jared Long <jlong4223@gmail.com>
2021-01-04 10:20:33 -0600

    restricts routes to temperatures and locations

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :temperatures
-  resources :locations
+  # using the only and key array, only allows users to access a couple routes even though there are more controllers for other routes
+  resources :temperatures, only: [:index, :create]
+  resources :locations, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
