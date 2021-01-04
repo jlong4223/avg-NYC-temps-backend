30cd4382696909b025409bf1a831b9dea4d37b47
jared Long <jlong4223@gmail.com>
2021-01-04 10:44:17 -0600

    includes location to the temperatures controller index

--- app/controllers/temperatures_controller.rb
+++ app/controllers/temperatures_controller.rb
@@ -1,17 +1,14 @@
 class TemperaturesController < ApplicationController
-  before_action :set_temperature, only: [:show, :update, :destroy]
+
 
   # GET /temperatures
   def index
     @temperatures = Temperature.all
 
-    render json: @temperatures
+    render json: @temperatures.to_json(include: :location)
   end
 
-  # GET /temperatures/1
-  def show
-    render json: @temperature
-  end
+
 
   # POST /temperatures
   def create
@@ -24,25 +21,11 @@ class TemperaturesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /temperatures/1
-  def update
-    if @temperature.update(temperature_params)
-      render json: @temperature
-    else
-      render json: @temperature.errors, status: :unprocessable_entity
-    end
-  end
 
-  # DELETE /temperatures/1
-  def destroy
-    @temperature.destroy
-  end
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_temperature
-      @temperature = Temperature.find(params[:id])
-    end
+
 
     # Only allow a list of trusted parameters through.
     def temperature_params
